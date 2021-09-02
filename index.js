9ff28205e8293290100279e7d4ce40ae2f12f223
Neil B. Chanez Gracia <Chanez89@Hotmail.com>
2021-09-01 22:42:13 -0700

    commit tree 1f2d57f3bb2da3103c414cf325390d56968a07ce

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+
+// function findMatching(drivers){
+// let driversName = drivers.filter(driver => driver.name === "Bobby");
+// }
+
+
+function findMatching(namesArray){
+    let matchingNames= namesArray.filter(name => names.result == 'Bobby');
+
+
+    if (matchingNames !== 'Bobby'){
+        return emtpyarray = []
+    }
+   
+   
+    return matchingNames;
+
+}      
+    
+    
+
+
+
+//fuzzyMatch()
+//matchName()
